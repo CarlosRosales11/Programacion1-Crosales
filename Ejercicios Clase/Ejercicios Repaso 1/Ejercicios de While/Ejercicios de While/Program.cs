79a43ecd3bc57c1f256ddd25f8264a967144e50c
CarlosRosales11 <carli.rosales28@gmail.com>
2023-04-20 18:27:46 -0300

    Cuarto Commit

--- Ejercicios Clase/Ejercicios Repaso 1/Ejercicios de While/Ejercicios de While/Program.cs
+++ Ejercicios Clase/Ejercicios Repaso 1/Ejercicios de While/Ejercicios de While/Program.cs
@@ -82,6 +82,63 @@ class listaNombres
 */
 
 // 4) Para calcular el total de una factura , el usuario ingresará pares de datos Costo y Total ; cuando se ingrese costo 0 finaliza el ingreso de datos; informar el total adeudado.
+/*
+class totalFactura
+{
+    static void Main(string[] args)
+    {
+        string costo;
+        string total;
+        int costoNum;
+        int suma = 0;
+        int totalNum;
 
+        Console.Write("Ingrese el costo: ");
+        costo = Console.ReadLine(); 
+        costoNum = int.Parse(costo);
+       
+        while (costoNum > 0)
+        {
+            Console.Write("Ingrese el total: ");
+            total = Console.ReadLine();
+            totalNum = int.Parse(total);
+
+            suma = suma + (costoNum * totalNum);
+
+            Console.Write("Ingrese el costo: ");
+            costo = Console.ReadLine();
+            costoNum = int.Parse(costo);
+        }
+        Console.WriteLine("El total adeudado es: " + suma);
+    }
+}
+*/
 
 // 5) Para calcular el consumo total de un cliente, el usuario primero ingresará cuántos productos compró; luego el sistema solicitará que ingrese el total y costo de cada uno de esos productos consumidos; al terminar informará el total adeudado por el cliente.
+/*
+class totalCliente
+{
+    static void Main(string[] args)
+    {
+        int cantidadProductos;
+        int costoProducto = 0;
+        int totalConsumido = 0;
+
+        Console.Write("Ingrese la cantidad de productos comprados: ");
+        cantidadProductos = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= cantidadProductos; i++)
+        {
+            Console.Write("Ingrese el total del producto: ");
+            int totalProductos = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el costo del producto: ");
+            costoProducto = int.Parse(Console.ReadLine());
+
+            totalConsumido += costoProducto;
+        }
+        Console.WriteLine("El total adeudado por el cliente es: " + totalConsumido);
+        Console.ReadLine();
+    }
+}
+*/
