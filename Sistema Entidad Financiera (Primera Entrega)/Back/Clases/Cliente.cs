84caaaa8d9239e597e635670f29c458e275c4e19
Carlos Rosales <101378676+CarlosRosales11@users.noreply.github.com>
2023-10-04 02:06:48 -0300

    Back

---
+++ Sistema Entidad Financiera (Primera Entrega)/Back/Clases/Cliente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Back.Clases
+{
+    public class Cliente
+    {
+        public Cliente(string nombre, string apellido, int dNI)
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+            DNI = dNI;
+        }
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int DNI { get; set; }
+        public string NombreCompleto { get { return Nombre + " " + Apellido; } }
+    }
+}
---
+++ Sistema Entidad Financiera (Primera Entrega)/Back/Clases/CuentaBancaria.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Back.Clases
+{
+    public class CuentaBancaria
+    {
+        [Key] public int NumeroCuenta { get; set; }
+        public double Saldo { get; set; }
+        public Cliente Titular { get; set; }
+        public string NombreTitular { get; set; }
+        public TipodeCuentaBancaria Tipo { get; set; }
+    }
+}
